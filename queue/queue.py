eebafe2082fbb0277fb4aac5afd7c7f328b914ca
AmarachiOE <ao.ejiawoko@gmail.com>
2019-07-22 10:55:15 -0700

    Added note to describe utility function

--- queue/queue.py
+++ queue/queue.py
@@ -5,11 +5,14 @@ class Queue:
     # use to store queue elements?
     self.storage = 
 
+  # add item to back of queue
   def enqueue(self, item):
     pass
   
+  # remove and return an item from the front of the queue
   def dequeue(self):
     pass
 
+  # returns the number of items in the queue
   def len(self):
     pass
