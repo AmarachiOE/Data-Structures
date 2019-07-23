74d844e100917102c29b151bfada3ec5e0c86e95
AmarachiOE <ao.ejiawoko@gmail.com>
2019-07-23 08:42:31 -0700

    Insert test passing for binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,28 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if value == self.value or self.value is None:
+      self.value = value
+
+    elif value < self.value:
+      if self.left is None:
+        self.left = BinarySearchTree(value)
+      else:
+        self.left.insert(value)
+
+    else: # value > self.value:
+      if self.right is None:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
 
   def contains(self, target):
-    pass
+    if self.value is None or self.value == target:
+      return True # return self.value
+
+    if self.value < target:
+      self.value = self.left
+      return contains(target)
 
   def get_max(self):
     pass
